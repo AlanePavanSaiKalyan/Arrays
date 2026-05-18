fc39a9f3dbc59287f6f4a475a20a7ae6e4c4b92a
Alane Pavan Sai Kalyan <alanepavansaikalyan3105@gmail.com>
2026-05-18 21:53:08 +0530

    Easy Addded

---
+++ src/Arrays_Easy.java
@@ -0,0 +1,75 @@
+import java.util.Arrays;
+import java.util.Vector;
+
+public class Arrays_Easy {
+    public static int getLargest(int []a){
+        int largest  = a[0];
+        for(int i =1;i<a.length;i++ ){
+            if(a[i]>largest){
+                largest = a[i];
+            }
+        }
+        return largest;
+    }
+
+    public static Integer getSecondLargest(Vector<Integer> a,int n){
+        Integer largest = a.getFirst();
+        Integer secondLargest = Integer.MIN_VALUE;
+        for(int i = 1;i<a.size();i++){
+            if(a.get(i)>largest){
+                secondLargest = largest;
+                largest = a.get(i);
+            }
+            else if(a.get(i)<largest && a.get(i)>secondLargest){
+                secondLargest = a.get(i);
+            }
+        }
+        return secondLargest;
+    }
+
+    public static Integer getSecondSmallest(Vector<Integer> a,int n){
+        int smallest = a.getFirst();
+        int secondSmallest = Integer.MAX_VALUE;
+        for(int i = 1;i<n;i++){
+            if(a.get(i)<smallest){
+                secondSmallest = smallest;
+                smallest = a.get(i);
+            } else if (a.get(i)>smallest && a.get(i)<secondSmallest) {
+                secondSmallest = a.get(i);
+            }
+        }
+        return  secondSmallest;
+    }
+
+//    public static int[]a removeDuplicates(int[] a){
+//    // Two pointer example
+//        int i = 0;
+//            for(int j = 1;i<a.length;i++){
+//                if(a[j]!=a[i]){
+//                    a[i+1]=a[j];
+//                    i++;
+//                }
+//            }
+//        return ;
+//    }
+
+    public static boolean isSorted(int []a){
+        for(int i = 0;i<a.length-1;i++){
+            if(a[i]>a[i+1]) return false;
+        }
+        return true;
+
+    }
+
+
+
+    public static Vector<Integer> getSeconOrderElements(Vector<Integer> a,int n){
+        int secondLargest = getSecondLargest(a,n);
+        int secondSmallest = getSecondSmallest(a,n);
+        return new Vector<>(Arrays.asList(secondLargest,secondSmallest));
+    }
+
+    static void main(String[] args) {
+        System.out.println(getSeconOrderElements(new Vector<>(Arrays.asList(6,1,7,6,2)),5));
+    }
+}
